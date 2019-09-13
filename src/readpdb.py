e453c339309414a608aab37b3e40a9204b04acc9
Myriam Hanna <myriamkhanna@gmail.com>
2019-09-13 23:20:43 +0200

    some fixes in class PDB plus added class Atom

--- src/readpdb.py
+++ src/readpdb.py
@@ -1,15 +1,21 @@
 import sys
-
+import math
+import os
 
 class Pdb:
 
-    def read_pdb(self):
+    def __init__(self, filename):
+        self.filename = filename
+        self.lines = self.read_pdb(filename)
+        self.dico = self.read_top()
+
+    def read_pdb(self, filename):
         with open(filename) as filin:
-            return(filin.readlines())
+            return filin.readlines()
 
     def read_top(self):
-        dico = {}
-        for line in self.rawlines:
+        for line in self.lines:
+            dico = {}
             if(line[0:10] == "HEADER    "):
                 dico['header'] = line
             elif(line[0:10] == "COMPND   2"):
@@ -17,15 +23,19 @@ class Pdb:
             elif(line[0:10] == "SOURCE   2"):
                 dico['source'] = line
             elif(line[0:10] == "AUTHOR   "):
-                dico['author'] = line
+                dico['author'] = lines
 
-    def __init__(self, filename):
-        self.rawlines = self.read_pdb(filename)
-        self.dico = self.read_top()
+
+class Atom:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def calcul_distance3D(self):
+        return math.sqrt((self.x - Atom.x)**2 + (self.y - Atom.y)**2 + (self.z - Atom.z)**2)
 
 
 if __name__ == '__main__':
-    if(len(sys.argv)<2):
-        print("not enough arguments")
-    else:
-        pdb = Pdb(sys.argv[1]).rawlines
+    pdb = Pdb(sys.argv[1]).lines
