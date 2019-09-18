20de5236b51ca0f0bcfb99a953b48dd9a9078dc2
Myriam Hanna <myriamkhanna@gmail.com>
2019-09-18 17:33:28 +0200

    Added find ladders

--- src/readpdb.py
+++ src/readpdb.py
@@ -9,14 +9,22 @@ class Pdb:
         self.lines = self.read_pdb(filename)
         #self.dico = self.read_top()
         self.content = {}
+        self.dict_nturns = {}
+        self.dict_bridges= {}
+        self.list_ladders = []
         self.atom_names = ['N', 'C', 'H01', 'O']
+        self.list_helices = []
+
 
     def read_pdb(self, filename):
+        """method """
         with open(filename) as filin:
             return filin.readlines()
 
 
+
     def get_info(self):
+        """ gets the most useful information from the PDB files: Res name, Res ID, and atom coordinates """
         cpt = 0
         mylist = []
         for line in self.lines:
@@ -39,10 +47,7 @@ class Pdb:
                 cpt += 1
 
     def energy_Hbonds(self, idx1, idx2):
-        """ method that return the energy between atoms that form Hbonds, dans ma tête cette fonction calcul les distances r_ON , r_CH ..
-        entre deux residus et genre on lappelle dans find n_turns et ca lutilise pour des résidus eloignés de 3, 4 ou 5 mais je sais pas
-        si c'est faisable
-        Une H_bond existe si E < 0.5  E = q1*q2*(1/r_ON + 1/r_CH - 1/r_OH - 1/r_CN)*f  """
+        """ method that returns the energy between atoms that form Hbonds"""
         q1 = 0.42
         q2 = 0.20
         f = 332
@@ -65,21 +70,21 @@ class Pdb:
 
 
     def find_nturns(self):
-        """ ici normalement ca devrait chercher chaque trois, 4, ou 5 residus sil y a une H bond yaane le 1er résidu avec le 3eme, le 1er
-        avec le 4eme, le 1 er avec le 5e, etc..  si il ya il la save qqpart"""
-        dict_nturns = {}
+        """ method that finds 3, 4, and 5 turns and save them in a dictionnary"""
+        #dict_nturns = {}
         for i, j in enumerate(list(self.content.keys())):
             for nb in range(3,6):
                 try:
                     if self.energy_Hbonds(i, i+nb ) < - 0.5:
-                        if ((j in dict_nturns and (nb - dict_nturns[j]) == 1) or (j not in dict_nturns)):
-                            dict_nturns[j] = nb
+                        if ((j in self.dict_nturns and (nb - self.dict_nturns[j]) == 1) or (j not in self.dict_nturns)):
+                            self.dict_nturns[j] = nb
                 except IndexError:
                     pass
 
-        print(3, self.count(dict_nturns, 3))
-        print(4, self.count(dict_nturns, 4))
-        print(5, self.count(dict_nturns, 5))
+        print(3, self.count(self.dict_nturns, 3))
+        print(4, self.count(self.dict_nturns, 4))
+        print(5, self.count(self.dict_nturns, 5))
+        #print(self.dict_nturns)
 
 
     def count(self, dict, value):
@@ -91,35 +96,68 @@ class Pdb:
 
 
     def find_bridges(self):
-        """ Deux sortes de bridges
-         bridge paralleles: h_bond(i-1,j) et h_bond(j,i+1) OU h_bond(j-1,i) et h_bond(i,j+1)
-        bridge anti paralleles: h_bond(i,j) et h_bond(j,i) OU h_bond(i-1, j+1) et h_bond(j-1, i+1)
+        """ method that searches for antiparallel and parallel bridges """
 
-        i et j sont deux résidus differents
-        """
-        dict_bridges= {}
-
-        for idx1, value1 in enumerate(list(self.content.keys())[1:-3]):
+        for idx1, value1 in enumerate(list(self.content.keys())[1:-1]):
             for idx2, value2 in enumerate(list(self.content.keys())[idx1+4:-1]):
                 try:
                     idc1 = idx1 + 1
                     idc2 = idx2 + idx1 + 4
-
                     if (((self.energy_Hbonds(idc1, idc2) < -0.5) and (self.energy_Hbonds(idc2 , idc1) < -0.5))
                         or ((self.energy_Hbonds(idc1-1, idc2+1) < -0.5) and (self.energy_Hbonds(idc2-1 , idc1+1) < -0.5))):
-                        dict_bridges[value1 +' '+ value2] = 'AP'
+                        self.dict_bridges[(value1, value2)] = 'AP'
 
                     elif (((self.energy_Hbonds(idc1-1, idc2) < -0.5) and (self.energy_Hbonds(idc2 , idc1+1) < -0.5))
                         or ((self.energy_Hbonds(idc2-1, idc1) < -0.5) and (self.energy_Hbonds(idc1 , idc2+1) < -0.5))):
-                        dict_bridges[value1 +' '+ value2] = 'P'
+                        self.dict_bridges[(value1, value2)] = 'P'
 
                 except IndexError:
                     pass
 
-        print('P', self.count(dict_bridges, 'P'))
-        print('AP', self.count(dict_bridges, 'AP'))
+        #print('P', self.count(self.dict_bridges, 'P'))
+        #print('AP', self.count(self.dict_bridges, 'AP'))
+        #print(self.dict_bridges)
+        self.find_ladders()
 
-#    def find_helices(self):
+    def find_ladders(self):
+        prev_key = list(self.dict_bridges.keys())[0]
+        prev_val = self.dict_bridges[prev_key]
+        count = 0
+        list_temp = []
+        for key, val in self.dict_bridges.items():
+            if count != 0:
+                if math.fabs(int(key[0]) - int(prev_key[0])) == 1 and math.fabs(int(key[1] )- int(prev_key[1])) == 1 and (val == prev_val):
+                    list_temp.extend([prev_key, key])
+                else:
+                    if len(list_temp) != 0:
+                        self.list_ladders.append(list(dict.fromkeys(list_temp)))
+                        list_temp = []
+            prev_key = key
+            prev_val = val
+            count += 1
+        print(self.list_ladders)
+
+    def find_helices(self):
+        prev_key = list(self.dict_nturns.keys())[0]
+        prev_val = self.dict_nturns[prev_key]
+        list_temp = []
+        for key, val in self.dict_nturns.items():
+            if (int(key) - int(prev_key)) == 1 and (val == prev_val):
+                list_temp.extend([prev_key, key, str(int(key)+1), str(int(key)+2)])
+            else:
+                if len(list_temp) != 0:
+                    self.list_helices.append(list(dict.fromkeys(list_temp)))
+                    list_temp = []
+            prev_key = key
+            prev_val = val
+        print(self.list_helices)
+
+
+
+
+
+
+
 
 
 
@@ -128,8 +166,6 @@ if __name__ == '__main__':
     #pdb=Pdb()
     #pdb.read_top()
     pdb.get_info()
-    #pdb.find_nturns()
+    pdb.find_nturns()
     pdb.find_bridges()
-
-    #print(pdb.content["16"][1])
-    #print(int(list(pdb.content.keys())[0]))
+    #pdb.find_helices()
